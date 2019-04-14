f22bc867bb166d53546fae7aa4c65a02c79f2300
Erind Hoxha <932400893@cit3-501m-22.local>
2019-04-14 17:12:30 +1200

    adding tvnz

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ setTimeout(() => {
 $("body").css('overflow', 'unset');
 $(".loader").fadeOut(300);
 startSVGAnimation($('#logo-front'));
-}, 2200);
+}, 00);
 
 $(".go-down, .cta-button").click(function() {
   $([document.documentElement, document.body]).animate({
