02631bf4b2383422f61b49ca81b288d82f456ca6
Erind Hoxha <erind_cbh@hotmail.com>
2020-09-26 21:11:36 +1200

    removed console.logss

--- js/main.js
+++ js/main.js
@@ -46,9 +46,7 @@ $(function () {
     var wintop = $(window).scrollTop(),
       docheight = $("body").height(),
       winheight = $(window).height();
-    console.log(wintop);
     var totalScroll = (wintop / (docheight - winheight)) * 100;
-    console.log("total scroll" + totalScroll);
     $("#KW_progressBar").css("width", totalScroll + "%");
 
     var scroll = $(window).scrollTop(); // how many pixels you've scrolled
